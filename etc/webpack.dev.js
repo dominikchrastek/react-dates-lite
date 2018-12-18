9830e05ed7e6bc552615e60920b44c016fcd0a36
halafi <filiphalas74@gmail.com>
2018-12-18 10:34:18 +0100

    refactor: make shit more explicit

--- etc/webpack.dev.js
+++ etc/webpack.dev.js
@@ -1,17 +1,20 @@
-const shared = require('./webpack.shared.js');
+const shared = require("./webpack.shared.js");
 
-const config = Object.assign(shared, {
-  mode: 'development',
+const config = {
+  entry: shared.entry,
+  resolve: shared.resolve,
+  module: shared.module,
+  mode: "development",
   output: {
-    filename: 'output.js',
-    publicPath: '/'
+    filename: "output.js",
+    publicPath: "/"
   },
   devServer: {
-    contentBase: 'static/',
+    contentBase: "static/",
     inline: true,
     historyApiFallback: true
   },
-  devtool: 'eval-source-map',
-});
+  devtool: "eval-source-map"
+};
 
 module.exports = config;
